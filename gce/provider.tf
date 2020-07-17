64ad1455434742ec67bbad7e88e2f846c2235f4d
Manjunath Reddy <manjunath.ramreddy@gmail.com>
2020-07-17 15:17:54 +0530

    changed the version details

--- gce/provider.tf
+++ gce/provider.tf
@@ -2,7 +2,9 @@ variable "path" {  default = "/home/cicd-user/gcp_credentials/keys" }
 
 provider "google" {
     project = "snappy-cosine-198513"
-    region = "asia-south1-a"
+    region = "asia-south1"
+    zone = "asia-south1-a"
+    version = "~> 2.17"
     credentials = "${file("${var.path}/credentials.json")}"
   
 }
