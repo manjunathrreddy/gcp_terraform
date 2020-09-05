202925b6ac5ddeeec32ea4e8b1c5bb8009f89389
Manjunath Reddy <manjunath.ramreddy@gmail.com>
2020-09-05 16:10:01 +0530

    test script

--- gce/provider.tf
+++ gce/provider.tf
@@ -1,10 +1,10 @@
 variable "path" {  default = "/home/cicd-user/gcp_credentials/keys" }
 
 provider "google" {
-    project = "snappy-cosine-198513"
+    project = "triple-virtue-271517"
     region = "asia-south1"
     zone = "asia-south1-a"
     version = "~> 2.17"
-    credentials = "${file("${var.path}/credentials.json")}"
+    credentials = "${file("${var.path}/triple-virtue.json")}"
   
 }
