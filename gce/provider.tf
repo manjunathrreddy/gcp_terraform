d3ca100a9464b6445b9d0984867a85c86da3e26e
Manjunath Reddy <manjunath.ramreddy@gmail.com>
2020-09-05 16:25:26 +0530

    test script

--- gce/provider.tf
+++ gce/provider.tf
@@ -4,7 +4,6 @@ provider "google" {
     project = "triple-virtue-271517"
     region = "asia-south1"
     zone = "asia-south1-a"
-    version = "~> 2.17"
     credentials = "${file("${var.path}/triple-virtue.json")}"
   
 }
