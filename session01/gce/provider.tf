feaff9942190e2ecd81e90d931be7fc96ef1fc6a
Manjunath Reddy <manjunath.ramreddy@gmail.com>
2020-09-06 09:06:59 +0530

    deleted files to test

--- session01/gce/provider.tf
+++
@@ -1,9 +0,0 @@
-variable "path" {  default = "/home/vagrant/gcp_credentials/keys" }
-
-provider "google" {
-    project = "triple-virtue-271517"
-    region = "asia-south1"
-    zone = "asia-south1-a"
-    credentials = "${file("${var.path}/triple-virtue.json")}"
-  
-}
