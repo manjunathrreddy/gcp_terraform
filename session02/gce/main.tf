93ed2a680217123878546e1991f849561607d288
Manjunath Reddy <manjunath.ramreddy@gmail.com>
2020-09-14 00:06:57 +0530

    changes to session 2

--- session02/gce/main.tf
+++ session02/gce/main.tf
@@ -1,7 +1,7 @@
 
 resource "google_compute_instance" "default" {
     
-    count = var.machine_count
+    count = length(var.name_count)
     name = "list-${count.index+1}"
     machine_type = var.environment != "production" ? var.machine_type : var.machine_type_dev
     metadata_startup_script = "${file("httpd_install.sh")}" 
