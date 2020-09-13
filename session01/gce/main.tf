91e9f9cd2efb65d31249d65391f8c8563f019fb3
Manjunath Reddy <manjunath.ramreddy@gmail.com>
2020-09-13 21:20:03 +0530

    changed zones

--- session01/gce/main.tf
+++ session01/gce/main.tf
@@ -3,6 +3,7 @@ variable "path" {  default = "/home/vagrant/gcp_credentials/keys" }
 
 provider "google" {
     project = "triple-virtue-271517"
+    version = "~> 3.38.0"
     region = "us-central1"
     zone = "us-central1-a"
     credentials = "${file("${var.path}/triple-virtue.json")}"
