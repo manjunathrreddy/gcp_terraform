984acd81cdb3edc7ae23afe22fae5d235525aec9
Manjunath Reddy <manjunath.ramreddy@gmail.com>
2020-09-14 00:20:56 +0530

    changes to session 2

--- session02/gce/variables.tf
+++ session02/gce/variables.tf
@@ -1,7 +1,7 @@
 variable "image" {  default = "centos-8" }
-variable "machine_type" { default = "n1-standard-1" }
+variable "machine_type" { default = "n1-standard-2" }
 variable "name_count" { default = ["server-1","server-2","server-3"]}
 variable "environment" { default = "production" }
-variable "machine_type_dev" { default = "n1-standard-4" }
+variable "machine_type_dev" { default = "n1-standard-1" }
 variable "machine_count" { default = "1" }
 variable "machine_size" { default = "20" }
