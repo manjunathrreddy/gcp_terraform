0d83203a3061090eeb3bcf02f89e2db05fc6900c
Manjunath Reddy <manjunath.ramreddy@gmail.com>
2020-09-14 00:24:37 +0530

    changes to session 2

--- session02/gce/main.tf
+++ session02/gce/main.tf
@@ -25,6 +25,9 @@ resource "google_compute_instance" "default" {
 
     network_interface {
         network = "default"
+        access_config {
+      // Ephemeral IP
+        }
     }
 
     metadata = {
