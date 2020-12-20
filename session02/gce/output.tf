3018622fb5969b6bc6ae038215b83a13ffad88eb
Manjunath Reddy <manjunath.ramreddy@gmail.com>
2020-12-20 20:25:43 +0530

    added output.tf

--- session02/gce/output.tf
+++ session02/gce/output.tf
@@ -1,7 +1,7 @@
 output "machine_type" {
-  value = "${google_compute_instance.test_instance[var.name_count+1].machine_type}"
+  value = "${google_compute_instance.test_instance[var.name_count].machine_type}"
 }
 
 output "name" {
-  value = "${google_compute_instance.test_instance[var.name_count+1].name}"
+  value = "${google_compute_instance.test_instance[var.name_count].name}"
 }
