0bd5136c12d9e4ecf917f8de415e687a0fe2593b
Manjunath Reddy <manjunath.ramreddy@gmail.com>
2021-02-14 08:24:44 +0530

    added the qa env

--- session03/app01/gce/provider.tf
+++ session03/app01/gce/provider.tf
@@ -1,9 +1,9 @@
-variable "path" {  default = "/home/vagrant/gcp_credentials/keys" }
+
 
 provider "google" {
     project = "${var.project_id}"
     region = "asia-south1"
     zone = "asia-south1-a"
-    credentials = "${file("${var.path}/triple-virtue.json")}"
+    credentials = "${credentials_file}"
   
 }
--- session03/app01/gce/variables.tf
+++ session03/app01/gce/variables.tf
@@ -11,3 +11,7 @@ variable "disk_name" { default = "" }
 variable "disk_size" { default = "" }
 
 variable "project_id" { default = "" }
+
+variable "credentials_file" { default = "" }
+
+variable "path" {  default = "/home/vagrant/gcp_credentials/keys" }
