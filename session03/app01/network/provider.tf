1c29c93d0fa8bca2cffb891c9a387826b2d31aec
Manjunath Reddy <manjunath.ramreddy@gmail.com>
2021-04-03 18:21:23 +0530

    updated the network configurations

---
+++ session03/app01/network/provider.tf
@@ -0,0 +1,9 @@
+
+
+provider "google" {
+    project = "${var.project_id}"
+    region = "asia-south1"
+    zone = "asia-south1-a"
+    credentials = "${file("${var.path}/${var.credentials_file}")}"
+  
+}
