429664d1753c433ca1d1c57981b0e7353234bd45
Manjunath R Reddy <manjunath.ramreddy@gmail.com>
2021-05-31 19:15:52 +0530

    added subnet module

---
+++ subnet/provider.tf
@@ -0,0 +1,7 @@
+provider "google" {
+    project = "gcp-homecompany-dev"
+    region = "us-central1"
+    zone = "us-central1-a"
+    credentials = "${file("${var.path}/${var.credentials_file}")}"
+  
+}
