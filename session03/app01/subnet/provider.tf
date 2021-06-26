66a99453f1e4f95e84958168203a640d3da0f127
Manjunath R Reddy <manjunath.ramreddy@gmail.com>
2021-06-26 17:58:46 +0530

    added the correct vpc details

--- session03/app01/subnet/provider.tf
+++ session03/app01/subnet/provider.tf
@@ -1,7 +1,5 @@
 provider "google" {
-    project = "gcp-homecompany-dev"
+    project = ["gcp-homecompany-dev","gcp-homecompany-qa"]
     region = "us-central1"
     zone = "us-central1-a"
-    credentials = "${file("${var.path}/${var.credentials_file}")}"
-  
 }
