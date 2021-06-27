6bce9d148e6d524307f157a91ac5d60f62f2c4f6
Manjunath R Reddy <manjunath.ramreddy@gmail.com>
2021-06-27 07:53:42 +0530

    added the credentials

--- session03/app01/subnet/provider.tf
+++ session03/app01/subnet/provider.tf
@@ -2,4 +2,5 @@ provider "google" {
     project = "gcp-homecompany-dev"
     region = "us-central1"
     zone = "us-central1-a"
+    credentials = "${file("${var.path}/${var.credentials_file}")}"
 }
